b15a222f9cab00e8af69ad19cafdbd83b6e01d16
sasxaxaxa <simsaaav@mail.ru>
2023-09-02 00:21:48 +0300

    writing code...

--- src/pack/lab1.java
+++ src/pack/lab1.java
@@ -1,36 +1,28 @@
 package pack;
 import java.util.Scanner;
-
 public class lab1 {
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int i = 2;
-//        System.out.println("Enter int num");
-//        if (sc.hasNextInt()) {
-//            i = sc.nextInt();
-//            System.out.println(i*2);
-//        }
-//        else {
-//            System.out.println("No int num");
-//        }
-//    }
-        String s1 = "firefox";
-        System.out.println(s1.toUpperCase());
-        String s2 = s1.replace('o', 'a');
-        System.out.println(s2); // выведет «firefax»
-        System.out.println(s2.charAt(1)); // выведет «i»
-        int i;
-        i = s1.length();
-        System.out.println(i);
-        i = s1.indexOf('f');
-        System.out.println(i);
-        i= s1.indexOf('r');
-        System.out.println(i);
-        i =s1.lastIndexOf('f');
-        System.out.println(i);
-        i = s1.indexOf('t');
-        System.out.println(i);
-        i = s1.indexOf('r',3);
-        System.out.println(i);
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        System.out.println("enter size of array");
+        if (sc.hasNextInt()) {
+            i = sc.nextInt();
+        }
+        else {
+            System.out.println("you didn't enter an integer");
+        }
+        int[] array;
+        array = new int[i];
+        for (int j = 0; j < i; j++) {
+            array[j] = sc.nextInt();
+        }
+        int sum = 0;
+        double everage;
+        for (int j = 0; j < i; j++) {
+            sum = sum + array[j];
+        }
+        everage = sum / i;
+        System.out.println("sum of elements=" + sum);
+        System.out.println("everage=" + everage);
     }
 }
