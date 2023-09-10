69f8276407a0cfc5966a3540ebf57cabf1b32cf6
sasxaxaxa <simsaaav@mail.ru>
2023-09-10 17:23:22 +0300

    coding...

--- src/pack/lab2/shop/Shop.java
+++ src/pack/lab2/shop/Shop.java
@@ -1,25 +1,59 @@
 package pack.lab2.shop;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Shop {
-    public Shop(String computer) {
-        this.computer = computer;
+
+    public void Adder(String device) {
+        devices.add(device);
     }
-
-    private String computer;
-    String[] arr;
-
-    public String[] getArr() {return arr;}
-    public void setArr() {this.arr = arr;}
-    public void Adder(String[] arr, String x) {
-        String[] new_arr = new String[arr.length + 1];
-        for (int i = 0; i < arr.length; i++) {
-            new_arr[i] = arr[i];
-
+    public void Remover(String device) {
+        devices.remove(device);
+    }
+    public void Searcher(String device) {
+        if (devices.contains(device)) {
+            System.out.println("Компьютер найден");
+        } else {
+            System.out.println("Компьютер не найден");
         }
-        new_arr[new_arr.length-1] = x;
-        arr = new_arr;
     }
 
+    static ArrayList<String> devices = new ArrayList<>();
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Shop shop = new Shop();
 
+        System.out.println("Добавление компьютеров. Для окончания ввода введите 0");
+        String device = sc.nextLine();
+        while (!device.equals("0")) {
+            shop.Adder(device);
+            device = sc.nextLine();
+        }
+        System.out.println("Ассортимент магазина:");
+        for (int i = 0; i < devices.size(); ++i) {
+            System.out.print(Shop.devices.get(i) + ' ');
+        }
+        System.out.println("\nЖелаете удалить компьютер? (1)");
+        int answer = sc.nextInt();
+        sc.nextLine();
+        if (answer == 1) {
+            System.out.println("Введите название:");
+            device = sc.nextLine();
+            shop.Remover(device);
+        }
+        System.out.println("Ассортимент магазина:");
+        for (int i = 0; i < devices.size(); ++i) {
+            System.out.print(Shop.devices.get(i) + ' ');
+        }
+        System.out.println("\nВыполнить поиск компьютера? (1)");
+        answer = sc.nextInt();
+        sc.nextLine();
+        if (answer == 1) {
+            System.out.println("Введите название:");
+            device = sc.nextLine();
+            shop.Searcher(device);
+        }
+    }
 }
