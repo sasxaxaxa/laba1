d02bd3cb49c07e193106680a880a9ae9847926f5
sasxaxaxa <simsaaav@mail.ru>
2023-09-11 23:41:24 +0300

    coding...

---
+++ src/pack/lab2/HowMany.java
@@ -0,0 +1,21 @@
+package pack.lab2;
+import java.nio.file.LinkPermission;
+import java.util.Scanner;
+public class HowMany {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int c = 0;
+        if (a.length() != 0) {
+            c++;
+            for (int i = 0; i < a.length(); i++) {
+                if(a.charAt(i) == ' ') {
+                    c++;
+                }
+            }
+        }
+        System.out.println(c + " слов(a)");
+
+    }
+
+}
