4ca5c99078d49fd8b96a092f495a3734370c8171
sasxaxaxa <simsaaav@mail.ru>
2023-09-11 15:22:12 +0300

    coding...

--- src/pack/lab2/poker/Poker.java
+++ src/pack/lab2/poker/Poker.java
@@ -1,50 +1,54 @@
 package pack.lab2.poker;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Poker {
+    public static int getRandomINumber(int i) {
+        return (int) (Math.random() * i);
+    }
+
     public static void main(String[] args) {
-        int cardsPerPlayer = 5;
-        int players = 0;
 
-        String[] suits = {
-                "Пик", "Бубен", "Черв", "Треф"
-        };
+        int cardNum = 5;
+        int players;
 
-        String[] rank = {
-                "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                "Валет", "Королева", "Король", "Туз"
-        };
+        String[] suits = {"\u2660", "\u2666", "\u2665", "\u2663"};
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Королева", "Король", "Туз"};
 
         int n = suits.length * rank.length; // количество карт
 
         Scanner sc = new Scanner(System.in);
         players = sc.nextInt();
+        String card;
+        ArrayList<String> array1 = new ArrayList<>();
 
-        // часть 3 - инициализация колоды
-        String[] deck = new String[n];
-        for (int i = 0; i < rank.length; i++) {
-            for (int j = 0; j < suits.length; j++) {
-                deck[suits.length * i + j] = rank[i] + " " + suits[j];
-            }
-        }
-
-//        for (int i = 0; i < rank.length; i++) {
-//            for (int j = 0; j < suits.length; j++) {
-//                System.out.println(deck[suits.length * i + j]);
-//            }
-//        }
-
-        // перетасовка колоды
         for (int i = 0; i < n; i++) {
-            int r = i + (int) (Math.random() * (n - i));
-            String temp = deck[r];
-            deck[r] = deck[i];
-            deck[i] = temp;
-        }
-        for (int i = 0; i < players * cardsPerPlayer; i++) {
-            System.out.println(deck[i]);
-            if (i % cardsPerPlayer == cardsPerPlayer - 1)
-                System.out.println();
-        }
-    }
 
+            int suitsrand = getRandomINumber(suits.length);
+            int rankrand = getRandomINumber(rank.length);
+            card = suits[suitsrand] + " " + rank[rankrand];
+
+            if (array1.contains(card)) {
+                do {suitsrand = getRandomINumber(suits.length);
+                    rankrand = getRandomINumber(rank.length);
+                    card = suits[suitsrand] + " " + rank[rankrand];}
+                while (array1.contains(card));
+                array1.add(card);
+            }
+            else {
+                array1.add(card);
+            }
+
+        }
+
+        for (int i = 0; i < cardNum*players; i++) {
+            if (i % 5 == 0) {
+                System.out.println();
+                System.out.println(i/5+1 + "-й игрок");
+            }
+            System.out.println(array1.get(i));
+
+        }
+
+    }
 }
