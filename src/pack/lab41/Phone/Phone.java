89b9e6a25b96f0cc1a97453071519e61d276b1e6
sasxaxaxa <simsaaav@mail.ru>
2023-09-16 12:45:41 +0300

    write code drink tea

--- src/pack/lab41/Phone/Phone.java
+++ src/pack/lab41/Phone/Phone.java
@@ -1,4 +1,48 @@
 package pack.lab41.Phone;
 
+import java.util.ArrayList;
+
 public class Phone {
+    public int number;
+    public String model;
+    public double weight;
+
+    public Phone(int number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void receiveCall(String name) {
+        System.out.printf("Звонит %s\n", name);
+    }
+
+    public void receiveCall(String name, int number) {
+        System.out.printf("Звонит %s с номера %d\n", name, number);
+    }
+
+
+    public Phone(int number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone() {
+    }
+
+    public void sendMessage(int number) {
+        System.out.printf("Сообщение отправлено номеру: %d\n", number);
+    }
 }
