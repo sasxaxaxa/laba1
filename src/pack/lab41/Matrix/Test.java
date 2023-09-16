04d94762d779042fe397eb97380cdfe650065a8d
sasxaxaxa <simsaaav@mail.ru>
2023-09-16 15:23:58 +0300

    write code drink tea

--- src/pack/lab41/Matrix/Test.java
+++ src/pack/lab41/Matrix/Test.java
@@ -13,27 +13,59 @@ public class Test {
         Integer[][] matrix1 = new Integer[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.printf("Введите элемент %d %d ->", i+1, j+1);
+                System.out.printf("Введите элемент %d %d ->", i + 1, j + 1);
                 int el = sc.nextInt();
                 matrix1[i][j] = el;
             }
         }
+
         Matrica matrica = new Matrica(matrix1);
 
-        System.out.println("Что нужно сделать с матрицей?");
-        String answer = sc.nextLine();
 
+        String answer = " ";
+        while (!answer.equals("")) {
+            System.out.println("Что нужно сделать с матрицей?\n1.Показать матрицу\n2.Сложить с другой матрицей\n3.Умножить на скаляр\n4.Умножить на другую матрицу");
+            sc.nextLine();
+            answer = sc.nextLine();
 
-        Integer[][] matrix2 = new Integer[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.printf("Введите элемент %d %d ->", i+1, j+1);
-                int el = sc.nextInt();
-                matrix2[i][j] = el;
+            switch (answer) {
+                case "1":
+                    matrica.Show(n, m);
+                    break;
+                case "2":
+                    System.out.println("Введите вторую матрицу");
+                    Integer[][] matrix2 = new Integer[n][m];
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < m; j++) {
+                            System.out.printf("Введите элемент %d %d ->", i + 1, j + 1);
+                            int el = sc.nextInt();
+                            matrix2[i][j] = el;
+                        }
+                    }
+                    matrica.Sum(matrix2, n, m);
+                    break;
+                case "3":
+                    System.out.println("Введите скаляр");
+                    int b = sc.nextInt();
+                    matrica.Scalar(b, n, m);
+                    break;
+                case "4":
+                    System.out.println("Введите вторую матрицу");
+                    Integer[][] matrix3 = new Integer[n][m];
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < m; j++) {
+                            System.out.printf("Введите элемент %d %d ->", i + 1, j + 1);
+                            int el = sc.nextInt();
+                            matrix3[i][j] = el;
+                        }
+                    }
+                    matrica.Proizv(matrix3, n, m);
+                    break;
+                case "":
+                    break;
+                default:
+                    System.out.println("Такой команды нет");
             }
         }
-        matrica.Sum(matrix2, n, m);
-
-
     }
 }
