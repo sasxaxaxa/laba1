24e7777975ae9e3b9f2cc7c6d5261590640210da
sasxaxaxa <simsaaav@mail.ru>
2023-09-17 16:52:10 +0300

    write code drink tea

---
+++ src/pack/lab6/Nameable3/Nameable.java
@@ -0,0 +1,83 @@
+package pack.lab6.Nameable3;
+
+public interface Nameable {
+    String name();
+}
+class Planet implements Nameable {
+    private String name;
+    public Planet(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Planet{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+    @Override
+    public String name() {
+        return name;
+    }
+}
+class Car implements Nameable {
+    private String name;
+    private String model;
+    public Car(String name, String model) {
+        this.name = name;
+        this.model = model;
+    }
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+    @Override
+    public String name() {
+        return name + " " + model;
+    }
+}
+
+class Pet implements Nameable {
+    private String name;
+    private String breed;
+
+    public Pet(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+    @Override
+    public String name() {
+        return name + " " + breed;
+    }
+}
+
+class Person implements Nameable {
+    private String name;
+    private String lastName;
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public String name() {
+        return name + " " + lastName;
+    }
+}
---
+++ src/pack/lab6/Nameable3/Names.java
@@ -0,0 +1,34 @@
+package pack.lab6.Nameable3;
+
+import pack.lab2.ball.Ball;
+
+import java.util.ArrayList;
+
+public class Names {
+    public static void main(String[] args) {
+        final ArrayList<Nameable> names = new ArrayList<>();
+
+        Planet saturn = new Planet("Saturn");
+        Planet earth = new Planet("Earth");
+
+        Pet barsik = new Pet("Barsik", "American");
+        Pet brus = new Pet("Brus", "Hungerian Vyzhla");
+
+        Person sasha = new Person("Sasha", "Aaa");
+
+        Car car = new Car("BMW", "X7");
+
+        names.add(saturn);
+        names.add(earth);
+        names.add(barsik);
+        names.add(brus);
+        names.add(sasha);
+        names.add(car);
+
+        for (Nameable nameable : names) {
+            System.out.println(nameable.toString());
+            System.out.println(nameable.name());
+        }
+
+    }
+}
