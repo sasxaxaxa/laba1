a6ce94162ff9c36c38dc44081a44754502b00b97
sasxaxaxa <simsaaav@mail.ru>
2023-09-19 15:11:16 +0300

    write code drink tea

---
+++ src/pack/lab8/ex6.java
@@ -0,0 +1,29 @@
+package pack.lab8;
+
+public class ex6 {
+    public static boolean recursion(int num, int del) {
+        if(del == num){
+            return true;
+        }
+        if (num % del == 0) {
+            return false;
+        }
+        return recursion(num, del + 1);
+    }
+
+    public static void main(String[] args) {
+//        int num = 15;
+//        int del = 2;
+//        System.out.println(recursion(num, del));
+        for (int i = 2; i < 34; i++) {
+            if (recursion(i, 2)) {
+                System.out.printf("Число %d простое\n", i);
+            }
+            else {
+                System.out.printf("Число %d сложное\n", i);
+            }
+        }
+
+
+    }
+}
