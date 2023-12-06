be31d7122d3eace2852492871609dca5c364457d
Alexandra  Simaeva <simsaaav@mail.ru>
2023-12-06 23:56:33 +0300

    write code drink tea

---
+++ src/pack/lab27/ex1/HashTab.java
@@ -0,0 +1,76 @@
+package pack.lab27.ex1;
+
+import java.util.Arrays;
+
+
+public class HashTab {
+    private int size = 10;
+    private Elem[] table;
+
+    public HashTab() {
+        table = new Elem[size];
+        Arrays.fill(table, null);
+    }
+
+    public class Elem {
+        private String key;
+        private int value;
+        private Elem nextelem = null;
+
+        public Elem(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int hashtabHash(String key) {
+        return key.hashCode() % size;
+    }
+
+    public void hashtabAdd(String key, int value) {
+        int index = hashtabHash(key);
+        Elem elem = new Elem(key, value);
+
+        if (table[index] == null) {
+            table[index] = elem;
+        } else {
+            Elem current = table[index];
+            while (current.nextelem != null) {
+                current = current.nextelem;
+            }
+            current.nextelem = elem;
+        }
+    }
+
+    public int hashtabLookup(String key) {
+        int index = hashtabHash(key);
+        Elem currentelem = table[index];
+
+        while (currentelem != null) {
+            if (currentelem.key.equals(key)) {
+                return currentelem.value;
+            }
+            currentelem = currentelem.nextelem;
+        }
+        return -1;
+    }
+
+    public void hashtabDelete(String key) {
+        int index = hashtabHash(key);
+        Elem currentelem = table[index];
+        Elem previouselem = null;
+
+        while (currentelem != null && !currentelem.key.equals(key)) {
+            previouselem = currentelem;
+            currentelem = currentelem.nextelem;
+        }
+
+        if (currentelem != null) {
+            if (previouselem == null) {
+                table[index] = currentelem.nextelem;
+            } else {
+                previouselem.nextelem = currentelem.nextelem;
+            }
+        }
+    }
+}
---
+++ src/pack/lab27/ex1/Tester.java
@@ -0,0 +1,24 @@
+package pack.lab27.ex1;
+
+public class Tester {
+    public static void main(String[] args) {
+        HashTab hashTab = new HashTab();
+
+        hashTab.hashtabAdd("positive", 1);
+        hashTab.hashtabAdd("negative", -2);
+        hashTab.hashtabAdd("positive", 100);
+        hashTab.hashtabAdd("negative", -200);
+
+        int find_elem = hashTab.hashtabLookup("positive");
+        System.out.println(find_elem);
+
+        hashTab.hashtabDelete("positive");
+        find_elem = hashTab.hashtabLookup("positive");
+        System.out.println(find_elem);
+
+        hashTab.hashtabDelete("positive");
+        find_elem = hashTab.hashtabLookup("positive");
+        System.out.println(find_elem);
+
+    }
+}
